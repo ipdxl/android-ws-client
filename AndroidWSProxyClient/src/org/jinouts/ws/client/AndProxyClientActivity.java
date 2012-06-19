a7f265f94d12561b7b84bb8940f94f7e4ff43b98
asraf344@gmail.com <asraf344@gmail.com@67c5326f-54a0-fc8f-ef0d-f7cf0db12ac0>
2012-06-19 15:13:16 +0000

    modified client

--- AndroidWSProxyClient/src/org/jinouts/ws/client/AndProxyClientActivity.java
+++ AndroidWSProxyClient/src/org/jinouts/ws/client/AndProxyClientActivity.java
@@ -1,17 +1,12 @@
 package org.jinouts.ws.client;
 
 import org.jinos.ws.client.R;
-import org.jinouts.webservice.service.Login;
-import org.jinouts.webservice.service.LoginService;
-import org.jinouts.webservice.service.LoginServiceResponse;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class AndProxyClientActivity extends Activity 
 {
@@ -33,47 +28,10 @@ public class AndProxyClientActivity extends Activity
     	Log.d ( "AndProxyClientActivity", "User and  Pass: "+userText.getText ( ).toString ( ) + " "+ passText.getText ( ).toString ( ) );
     	
     	
-    	LoginServiceTask loginTask = new LoginServiceTask ( );
-    	loginTask.execute ( new Void[]{} );
+    	/*LoginServiceTask loginTask = new LoginServiceTask ( );
+    	loginTask.execute ( new Void[]{} );*/
     	
     }
   
-    private class LoginServiceTask extends AsyncTask<Void, Void, LoginServiceResponse>
-    {
-		@Override
-		protected LoginServiceResponse doInBackground ( Void... params )
-		{
-			LoginService service = new LoginService ( );
-			Login login = service.getLoginPort ( );
-			
-			LoginServiceResponse resp = login.login ( "asraf", "asraf" );
-			
-			return resp;
-		}
-		
-		@Override
-		protected void onPostExecute(LoginServiceResponse resp)
-        {        	
-        	TextView respTextView = (TextView)findViewById ( R.id.respTextView );
-        	if ( resp != null )
-        	{
-        		Log.i (  "AndroidWSAppActivity ", "Success Code From Resp !!!!!!: "+ resp.getSuccessCode ( ) );
-            	Log.i (  "AndroidWSAppActivity ", "Error Code From Resp !!!!!!: "+ resp.getErrorCode ( ) );
-            	
-        		respTextView.setText ( "SuccessCode: "+resp.getSuccessCode ( ) + " Message: "+resp.getSuccessMessage ( ) 
-            			+" \nErrorCode: "+resp.getErrorCode ( ) + " Message: "+resp.getErrorMessage ( ) );
-    	        
-        	}
-        	else
-        	{
-        		respTextView.setText ( "Couldn't get Response" );    	        
-        	}
-        	
-        	
-        }
-    	
-    }
-   
-
 	
 }
